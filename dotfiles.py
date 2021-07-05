1d3254805f984de7fc225e278d2fc70249bfdcae
amkdg <mukhamedgaliyevali@gmail.com>
2021-07-05 08:03:21 -0700

    Fixed a typo

--- dotfiles.py
+++ dotfiles.py
@@ -7,9 +7,10 @@ project_directory = os.path.dirname(os.path.abspath(__file__))
 dotfiles = os.listdir(f'{project_directory}/Dotfiles')
 
 for dotfile in dotfiles:
-    if not os.path.isdir:
+    absolute_dotfile = f'{project_directory}/Dotfiles/{dotfile}'
+
+    if not os.path.isdir(absolute_dotfile):
         try:
-            absolute_dotfile = f'{project_directory}/Dotfiles/{dotfile}'
             dotfile_target_path = f'{os.path.expanduser("~")}/{dotfile}'
 
             os.symlink(absolute_dotfile, dotfile_target_path)
